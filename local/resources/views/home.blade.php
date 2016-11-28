ae63b7b6479b390c891c441943b25d6c7617d5a3
Tomkilica <tomislava.zivojinovic@gmail.com>
2016-11-28 16:50:24 +0100

    cover img

--- local/resources/views/home.blade.php
+++ local/resources/views/home.blade.php
@@ -7,34 +7,51 @@
 </head>
 
 @section('content')
-	<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 520px; overflow: hidden; visibility: hidden;">
+<!-- 	<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 520px; overflow: hidden; visibility: hidden;">
 	    <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
 	        <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
 	        <!-- <div style="position:absolute;display:block;background:url('./local/resources/assets/img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div> -->
-	    </div>
-	    <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 520px; overflow: hidden;">
+	    <!-- </div> -->
+	    <!-- div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 520px; overflow: hidden;">
 	        <div data-p="225.00" style="display: none;">
 	            <img data-u="image" src="./local/resources/assets/img/home/imgherofirst.jpg" />
 	            <div style="position: absolute; top: 25%; left: 30px; left: 50%; width: 440px; height: 60px;margin-left:-220px;font-size: 40px; color: #ffffff; line-height: 60px;">We Analyze, Design and</div>
 	            <div style="position: absolute; top: 35%; left: 50%; width: 630px; height: 50px;margin-left:-306px; font-size: 40px; color: #ffffff;">Implement Digital Health Solutions</div>
 	            <div style="position: absolute; top: 45%; left: 50%; width: 590px; height: 30px;margin-left:-273px; font-size: 20px; color: #ffffff;">Translating business requirements into digital health solutions</div>
         	 	<a href=""><div id="button" class="button"><span>Learn More</span></div></a>
- 	        </div>
+ 	        </div> -->
 	    <!--     <div data-p="225.00" style="display: none;">
 	            <img data-u="image" src="./local/resources/assets/img/home/1.jpg" />
 	        </div>
 	        <div data-p="225.00" style="display: none;">
 	            <img data-u="image" src="./local/resources/assets/img/home/2.jpg" />
 	        </div> -->
-	    </div>
+	    <!-- </div> -->
 	    <!-- Bullet Navigator 
 	    <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
 	        <div data-u="prototype" style="width:16px;height:16px;"></div>
 	    </div>-->
 	    <!-- Arrow Navigator -->
-	    <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
+	   <!--  <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
 	    <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
 	    <a href="http://www.jssor.com" style="display:none">Bootstrap Carousel</a>
+	</div> --> 
+	<div class="cover">
+	<div class="cover-img">
+	            <img data-u="image" src="./local/resources/assets/img/home/imgherofirst.jpg">
+	            <div style="position: absolute; top: 25%; left: 30px; left: 50%; width: 440px; height: 60px;margin-left:-220px;font-size: 40px; color: #ffffff; line-height: 60px;">We Analyze, Design and</div>
+	            <div style="position: absolute; top: 35%; left: 50%; width: 630px; height: 50px;margin-left:-306px; font-size: 40px; color: #ffffff;">Implement Digital Health Solutions</div>
+	            <div style="position: absolute; top: 45%; left: 50%; width: 590px; height: 30px;margin-left:-273px; font-size: 20px; color: #ffffff;">Translating business requirements into digital health solutions</div>
+        	 	<a href=""><div id="button" class="button"><span>Learn More</span></div></a>
+ 	        <div data-u="loading" style="position: absolute; top: 0px; left: 0px; width: 1300px; height: 520px; z-index: 1000; display: none;">
+	        <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+	        <!-- <div style="position:absolute;display:block;background:url('./local/resources/assets/img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div> -->
+	    </div></div> 
+		<!-- <img src="./local/resources/assets/img/home/imgherofirst1.jpg"> -->
+		<!-- <div >We Analyze, Design and</div>
+	           <div >Implement Digital Health Solutions</div>
+	            <div>Translating business requirements into digital health solutions</div>
+        	 	<a href=""><div id="button" class="button"><span>Learn More</span></div></a -->
 	</div>
 	<div class="who-we-are">			
 		<div class="box">	
